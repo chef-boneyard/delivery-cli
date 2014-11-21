aef07581fb26f28dc78ccba8213515ec169cc820
Adam Jacob <adam@opscode.com>
2014-11-21 11:15:15 -0500

    Lets clean ourselves up first

--- cookbooks/delivery_rust/recipes/syntax.rb
+++ cookbooks/delivery_rust/recipes/syntax.rb
@@ -1,3 +1,7 @@
+delivery_builder_exec "cargo clean" do
+  cwd node['delivery_builder']['repo']
+end
+
 delivery_builder_exec "cargo build" do
-  cwd repo
+  cwd node['delivery_builder']['repo']
 end
--- cookbooks/delivery_rust/recipes/unit.rb
+++ cookbooks/delivery_rust/recipes/unit.rb
@@ -1,3 +1,7 @@
+delivery_builder_exec "cargo clean" do
+  cwd node['delivery_builder']['repo']
+end
+
 delivery_builder_exec "cargo test" do
   cwd node['delivery_builder']['repo']
 end
