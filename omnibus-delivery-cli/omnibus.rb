b05c467243a079d73bd8e531bb7193aff1e7a248
Thom May <thom@may.lt>
2015-08-25 15:47:23 +0100

    use s3 cache, and reenable local builds

--- omnibus-delivery-cli/omnibus.rb
+++ omnibus-delivery-cli/omnibus.rb
@@ -13,7 +13,7 @@
 #
 # Uncomment this line to change the default base directory to "local"
 # -------------------------------------------------------------------
-# base_dir './local'
+base_dir './local'
 #
 # Alternatively you can tune the individual values
 # ------------------------------------------------
@@ -30,7 +30,7 @@
 
 # Enable S3 asset caching
 # ------------------------------
-# use_s3_caching true
+use_s3_caching true
 # s3_access_key  ENV['AWS_ACCESS_KEY_ID']
 # s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
 s3_bucket      ENV['AWS_S3_BUCKET'] || 'opscode-omnibus-cache'
