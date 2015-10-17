c46b96fe68c5d211e6891455f8007364451cb336
Matt Campbell <mcampbell@chef.io>
2015-10-17 12:40:31 -0700

    Use languages cookbook to install Rust on Windows too

--- cookbooks/delivery_rust/recipes/default.rb
+++ cookbooks/delivery_rust/recipes/default.rb
@@ -8,19 +8,12 @@ include_recipe 'chef-sugar::default'
 
 include_recipe "delivery_rust::_prep_builder"
 
+rust_version = node['delivery_rust']['rust_version']
 
-if windows?
-  windows_package "rust" do
-    source "https://static.rust-lang.org/dist/#{node['delivery_rust']['rust_version']}/rust-nightly-x86_64-pc-windows-gnu.msi"
-  end
-else
-  rust_version = node['delivery_rust']['rust_version']
-
-  rust_install "Install #{rust_version}" do
-    channel "nightly"
-    version rust_version
-    prefix "/usr/local"
-  end
+rust_install "Install #{rust_version}" do
+  channel 'nightly'
+  version rust_version
+  prefix '/usr/local' unless windows?
 end
 
 include_recipe "delivery_rust::_openssl"
