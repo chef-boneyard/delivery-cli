1382871072a65ea91c207f710a9be829c96016d3
Oliver Ferrigni <oliver@chef.io>
2015-10-18 21:56:15 -0500

    Depend on languages

--- cookbooks/delivery_rust/recipes/default.rb
+++ cookbooks/delivery_rust/recipes/default.rb
@@ -14,24 +14,12 @@ if windows?
     source "https://static.rust-lang.org/dist/#{node['delivery_rust']['rust_version']}/rust-nightly-x86_64-pc-windows-gnu.msi"
   end
 else
-  cache_dir = Chef::Config[:file_cache_path]
-
-  remote_file "#{cache_dir}/rustup.sh" do
-    source "https://static.rust-lang.org/rustup.sh"
-  end
-
   rust_version = node['delivery_rust']['rust_version']
-  rustup_cmd = ["bash",
-                "#{cache_dir}/rustup.sh",
-                "--channel=nightly",
-                "--date=#{rust_version}",
-                "--yes"].join(' ')
 
-  rustup_cmd << " --disable-sudo" if platform_family?('mac_os_x')
-
-  execute "install rust and cargo" do
-    command rustup_cmd
-    not_if { rust_version == current_rust_version }
+  rust_install "Install #{rust_version}" do
+    channel "nightly"
+    version rust_version
+    prefix "/usr/local"
   end
 end
 
