7c14dd1fa93c700d86653000d608f3c84ee15839
Matt Campbell <mcampbell@chef.io>
2015-11-02 17:35:23 -0600

    Consolidate windows conditions

--- cookbooks/delivery_rust/attributes/default.rb
+++ cookbooks/delivery_rust/attributes/default.rb
@@ -1,7 +1,9 @@
 default['delivery_rust']['rust_version'] = '2015-10-03'
-override['omnibus']['ruby_version'] = '2.1.6-x64' if node['platform_family'] == 'windows'
-# Working around a weird bug in the 7-zip cookbook where sometimes the installer ignores INSTALLDIR
-override['7-zip']['home'] = 'C:\\Program Files\\7-Zip' if node['platform_family'] == 'windows'
+if platform_family == 'windows'
+  override['omnibus']['ruby_version'] = '2.1.6-x64'
+  # Working around a weird bug in the 7-zip cookbook where sometimes the installer ignores INSTALLDIR
+  override['7-zip']['home'] = 'C:\\Program Files\\7-Zip'
+end
 
 openssl_version = '1.0.1p'
 default['delivery_rust']['windows']['openssl_url']         = "https://github.com/jaym/windows-openssl-build/releases/download/openssl-#{openssl_version}/openssl-#{openssl_version}-x64-windows.tar.lzma"
--- cookbooks/delivery_rust/metadata.rb
+++ cookbooks/delivery_rust/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adam@getchef.com'
 license          'All Rights Reserved'
 description      'Installs/Configures delivery_rust'
 long_description 'Installs/Configures delivery_rust'
-version          '0.3.4'
+version          '0.3.5'
 
 depends 'apt'
 depends 'build-essential'
