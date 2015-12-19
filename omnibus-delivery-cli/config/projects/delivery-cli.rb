64fa66650aea7b23a0c581e19fd7caca7d47ba27
Seth Chisamore <schisamo@chef.io>
2015-12-19 10:01:14 -0500

    Enable RPM, PKG and MSI signing!

--- omnibus-delivery-cli/config/projects/delivery-cli.rb
+++ omnibus-delivery-cli/config/projects/delivery-cli.rb
@@ -36,6 +36,19 @@ dependency "version-manifest"
 exclude "**/.git"
 exclude "**/bundler/git"
 
+package :rpm do
+  signing_passphrase ENV['OMNIBUS_RPM_SIGNING_PASSPHRASE']
+end
+
+package :pkg do
+  identifier "io.chef.pkg.delivery-cli"
+  signing_identity "Developer ID Installer: Chef Software, Inc. (EU3VF8YLX2)"
+end
+compress :dmg
+
 package :msi do
+  fast_msi true
   upgrade_code "178C5A9A-3923-4A65-AECB-3851224D0FDD"
+  wix_candle_extension 'WixUtilExtension'
+  signing_identity "F74E1A68005E8A9C465C3D2FF7B41F3988F0EA09", machine_store: true
 end
