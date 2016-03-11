f0b62cdd96d921e313a3159cc9f08205b3a300b3
Salim Afiune <afiune@chef.io>
2016-03-10 21:17:21 -0500

    Add version-bumping support
    Add `bump_version` fn that will:
    * Detect when you are reviewing a cookbook
    * Verify the current version with the version that is in the pipeline
    * If the versions are the same, it will bump the patch version and
    * create a commit with the message: `Bump version to A.B.(Z+1)`
    
    This will only happens when `delivery review` is executed.

--- src/delivery/cli/mod.rs
+++ src/delivery/cli/mod.rs
@@ -3,16 +3,16 @@ use std::process;
 use std::process::Command;
 use std::process::Stdio;
 use std::error::Error;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::fs::File;
 use std::error;
-use std::path::Path;
 use std::io::prelude::*;
 use std;
 
 use utils::{self, privileged_process};
 
 use utils::say::{self, say, sayln};
-use utils::mkdir_recursive;
+use utils::{mkdir_recursive, read_file};
 use errors::{DeliveryError, Kind};
 use config::Config;
 use delivery_config::DeliveryConfig;
@@ -25,7 +25,8 @@ use http::{self, APIClient};
 use hyper::status::StatusCode;
 use project;
 use clap::{Arg, App, SubCommand, ArgMatches};
-use utils::path_ext::is_dir;
+use utils::path_ext::{is_dir, is_file};
+use regex::Regex;
 
 macro_rules! make_arg_vec {
     ( $( $x:expr ),* ) => {
@@ -392,18 +393,98 @@ fn clap_review(matches: &ArgMatches) -> Result<(), DeliveryError> {
     review(pipeline, &no_open, &edit)
 }
 
+// Return the path to the metadata.rb file
+fn metadata_file(path: &PathBuf) -> String {
+    let mut metadata = path.to_str().unwrap().to_string();
+    metadata.push_str("/metadata.rb");
+    return metadata
+}
+
+// Vefiry if the provided path is a cookbook, or not
+fn is_cookbook(path: &PathBuf) -> bool {
+    let meta_f = metadata_file(path);
+    is_file(&Path::new(&meta_f))
+}
+
+// Extract the cookbook version from the provided metadata content
+//
+// This function expects you to read the metadata in advance and
+// pass just the content of the file. From there it will read every
+// line and search for the version to return it.
+fn metadata_version_from(content: String) -> Result<String, DeliveryError> {
+    for l in content.lines() {
+        let r = Regex::new(r"version\s+'(?P<version>[0-9]*\.[0-9]*\.[0-9]*)'").unwrap();
+        match r.captures(l) {
+            None => {},
+            Some(version) => return Ok(version.name("version").unwrap().to_string())
+        };
+    };
+    return Err(DeliveryError{ kind: Kind::MissingMetadataVersion, detail: None })
+}
+
+// Bump the metadata version, only if:
+// * The project is a cookbook
+// * The version hasn't been updated
+//
+// @param p_root [&PathBuf] The project root path
+// @param pipeline [&str] Pipeline the change is targeting to
+// @return ()
+fn bump_version(p_root: &PathBuf, pipeline: &str) -> Result<(), DeliveryError> {
+    if is_cookbook(&p_root) {
+        let project = try!(project_from_cwd());
+        say("white", "Project ");
+        say("yellow", &project);
+        sayln("white", " is a cookbook");
+        sayln("white", "Validating version in metadata");
+
+        let meta_f_p = PathBuf::from(metadata_file(&p_root));
+        let current_v = try!(metadata_version_from(try!(read_file(&meta_f_p))));
+        let t_file = [pipeline, ":metadata.rb"].concat();
+        let pipeline_meta = try!(git::git_command(&["show", &t_file], &p_root));
+        let pipeline_v = try!(metadata_version_from(pipeline_meta.stdout));
+
+        if current_v == pipeline_v {
+            say("yellow", "The version hasn't been updated (");
+            say("red", &pipeline_v);
+            sayln("yellow", ")");
+            let s = pipeline_v.split(".");
+            let vec: Vec<&str> = s.collect();
+            let patch = vec[2].parse::<u32>().unwrap();
+            let new_patch: String = (patch + 1).to_string();
+            let new_version = [vec[0], ".", vec[1], ".", new_patch.trim()].concat();
+            say("white", "Bumping version to: ");
+            sayln("green", &new_version);
+
+            let current_meta = try!(read_file(&meta_f_p));
+            let new_meta = current_meta.replace(&*current_v, &*new_version);
+
+            // Recreate the file and dump the processed contents to it
+            let mut recreate_meta = try!(File::create(metadata_file(&p_root)));
+            try!(recreate_meta.write_fmt(format_args!("{}", &new_meta)));
+
+            let commit_msg = ["Bump version to ", new_version.trim()].concat();
+            try!(git::git_command(&["add", &metadata_file(&p_root)], &p_root));
+            try!(git::git_command(&["commit", "-m", &commit_msg], &p_root));
+        } else {
+            say("white", "Version already updated (");
+            say("magenta", &pipeline_v);
+            say("white", "/");
+            say("green", &current_v);
+            sayln("white", ")");
+        }
+    }
+    Ok(())
+}
+
 fn review(for_pipeline: &str,
           no_open: &bool, edit: &bool) -> Result<(), DeliveryError> {
     sayln("green", "Chef Delivery");
     let mut config = try!(load_config(&cwd()));
     config = config.set_pipeline(for_pipeline);
     let target = validate!(config, pipeline);
-    // validate the delivery config file
-    // TODO: same as elsewhere in the code, we should get the project's root
-    // (instead of simply cwd), e.g. by looking for the .git dir?
-    let cwd = try!(env::current_dir());
-    try!(DeliveryConfig::validate_config_file(&cwd));
-
+    let project_root = try!(project::root_dir(&cwd()));
+    try!(DeliveryConfig::validate_config_file(&project_root));
+    try!(bump_version(&project_root, &target));
     say("white", "Review for change ");
     let head = try!(git::get_head());
     if &target == &head {
