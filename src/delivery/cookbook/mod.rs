70ce8f872fff041a54e9395ebd4979e21b137f45
Salim Afiune <afiune@chef.io>
2016-03-16 18:00:01 -0400

    Add struct MetadataVersion
    This new struct is the representation of version inside a metadata file,
    you can use it as follow:
    ```
    use cookbook;
    
    let my_version = cookbook::MetadataVersion { major: 1, minor: 1, patch: 0 }
    ```
    
    Now the `cookbook` module uses this struct to manipulate the metadata
    version.

--- src/delivery/cookbook/mod.rs
+++ src/delivery/cookbook/mod.rs
@@ -26,6 +26,69 @@ use git;
 use project;
 use regex::Regex;
 
+#[derive(Debug, Clone)]
+pub struct MetadataVersion {
+    pub major: usize,
+    pub minor: usize,
+    pub patch: usize,
+}
+
+impl MetadataVersion {
+    pub fn to_string(&self) -> String {
+        [
+            self.major.to_string(), ".".to_string(),
+            self.minor.to_string(), ".".to_string(),
+            self.patch.to_string()
+        ].concat()
+    }
+}
+
+// Bump the metadata version, only if:
+// * The project is a cookbook
+// * The version hasn't been updated
+//
+// @param p_root [&PathBuf] The project root path
+// @param pipeline [&str] Pipeline the change is targeting to
+// @return () if success
+pub fn bump_version(p_root: &PathBuf, pipeline: &str) -> Result<(), DeliveryError> {
+    if is_cookbook(&p_root) {
+        let project = try!(project::project_from_cwd());
+        say("white", "Project ");
+        say("yellow", &project);
+        sayln("white", " is a cookbook");
+        sayln("white", "Validating version in metadata");
+
+        let meta_f_p = PathBuf::from(metadata_file(&p_root));
+        let current_meta_v = try!(metadata_version_from(&try!(read_file(&meta_f_p))));
+        let current_v = current_meta_v.to_string();
+        let mut t_file = pipeline.to_string();
+        t_file.push_str(":metadata.rb");
+        let pipeline_meta = try!(git::git_command(&["show", &t_file], &p_root));
+        let pipeline_meta_v = try!(metadata_version_from(&pipeline_meta.stdout));
+        let pipeline_v = pipeline_meta_v.to_string();
+
+        if current_v == pipeline_v {
+            say("yellow", "The version hasn't been updated (");
+            say("red", &pipeline_v);
+            sayln("yellow", ")");
+            let new_meta_version = try!(bump_patchset(pipeline_meta_v.clone()));
+            let new_version = new_meta_version.to_string();
+            say("white", "Bumping version to: ");
+            sayln("green", &new_version);
+            try!(save_version(&meta_f_p, new_version));
+        } else {
+            say("white", "Version already updated (");
+            say("magenta", &pipeline_v);
+            say("white", "/");
+            say("green", &current_v);
+            sayln("white", ")");
+        }
+    }
+    Ok(())
+}
+
+// @Private
+
 // Return the path to the metadata.rb file
 fn metadata_file(path: &PathBuf) -> String {
     let mut metadata = path.to_str().unwrap().to_string();
@@ -44,39 +107,46 @@ fn is_cookbook(path: &PathBuf) -> bool {
 // This function expects you to read the metadata in advance and
 // pass just the content of the file. From there it will read every
 // line and search for the version to return it.
-fn metadata_version_from(content: String) -> Result<String, DeliveryError> {
+//
+// There are two valid version formats:
+// a) 'x.y.z' - The normal semantic version. (mayor.minor.patch)
+// b) 'x.y'   - Where the patchset will be 0 by default. (mayor.minor.0)
+fn metadata_version_from(content: &String) -> Result<MetadataVersion, DeliveryError> {
     for l in content.lines() {
-        let r = Regex::new(r"version\s+'(?P<version>[0-9]*\.[0-9]*\.[0-9]*)'").unwrap();
-        if let Some(version) = r.captures(l) {
-            return Ok(version.name("version").unwrap().to_string())
-        };
+        let r_m_m_p = Regex::new(r"version\s+'(?P<major>\d*)\.(?P<minor>\d*)\.(?P<patch>\d*)'").unwrap();
+        if let Some(version) = r_m_m_p.captures(l) {
+            let ma: usize = try!(version.name("major").unwrap().parse());
+            let mi: usize = try!(version.name("minor").unwrap().parse());
+            let pa: usize = try!(version.name("patch").unwrap().parse());
+            return Ok(MetadataVersion { major: ma, minor: mi, patch: pa });
+        }
+        let r_m_m = Regex::new(r"version\s+'(?P<major>\d+)\.(?P<minor>\d+)'").unwrap();
+        if let Some(version) = r_m_m.captures(l) {
+            let ma: usize = try!(version.name("major").unwrap().parse());
+            let mi: usize = try!(version.name("minor").unwrap().parse());
+            let pa: usize = 0;
+            return Ok(MetadataVersion { major: ma, minor: mi, patch: pa })
+        }
     };
     return Err(DeliveryError{ kind: Kind::MissingMetadataVersion, detail: None })
 }
 
 // Bump the patchset of the provided version
-fn bump_patchset(version: String) -> Result<String, DeliveryError> {
-    let s = version.split(".");
-    let m_m_p: Vec<&str> = s.collect();
-    let patch = try!(m_m_p[2].parse::<u32>());
-    let new_patch: String = (patch + 1).to_string();
-    let mut new_version: String = m_m_p[0].to_string();
-    new_version.push_str(".");
-    new_version.push_str(m_m_p[1]);
-    new_version.push_str(".");
-    new_version.push_str(&new_patch);
-    Ok(new_version)
+fn bump_patchset(mut version: MetadataVersion) -> Result<MetadataVersion, DeliveryError> {
+    version = MetadataVersion { patch: version.patch + 1, .. version };
+    Ok(version)
 }
 
 // Saves the new version to the metadata and commit the changes
 fn save_version(metadata: &PathBuf, version: String) -> Result<(), DeliveryError> {
     let current_meta = try!(read_file(metadata));
-    let current_version = try!(metadata_version_from(current_meta.clone()));
+    let current_meta_version = try!(metadata_version_from(&current_meta));
+    let current_version = current_meta_version.to_string();
     let new_metadata = current_meta.replace(&*current_version, &*version);
 
     // Recreate the file and dump the processed contents to it
     let mut recreate_meta = try!(File::create(metadata));
-    try!(recreate_meta.write_fmt(format_args!("{}", &new_metadata)));
+    try!(recreate_meta.write(new_metadata.as_bytes()));
 
     // Commit the changes made to the metadata
     let mut commit_msg = String::from("Bump version to ");
@@ -86,62 +156,70 @@ fn save_version(metadata: &PathBuf, version: String) -> Result<(), DeliveryError
     Ok(())
 }
 
-// @Public
-
-// Bump the metadata version, only if:
-// * The project is a cookbook
-// * The version hasn't been updated
-//
-// @param p_root [&PathBuf] The project root path
-// @param pipeline [&str] Pipeline the change is targeting to
-// @return () if success
-pub fn bump_version(p_root: &PathBuf, pipeline: &str) -> Result<(), DeliveryError> {
-    if is_cookbook(&p_root) {
-        let project = try!(project::project_from_cwd());
-        say("white", "Project ");
-        say("yellow", &project);
-        sayln("white", " is a cookbook");
-        sayln("white", "Validating version in metadata");
-
-        let meta_f_p = PathBuf::from(metadata_file(&p_root));
-        let current_v = try!(metadata_version_from(try!(read_file(&meta_f_p))));
-        let mut t_file = pipeline.to_string();
-        t_file.push_str(":metadata.rb");
-        let pipeline_meta = try!(git::git_command(&["show", &t_file], &p_root));
-        let pipeline_v = try!(metadata_version_from(pipeline_meta.stdout));
-
-        if current_v == pipeline_v {
-            say("yellow", "The version hasn't been updated (");
-            say("red", &pipeline_v);
-            sayln("yellow", ")");
-            let new_version = try!(bump_patchset(pipeline_v));
-            say("white", "Bumping version to: ");
-            sayln("green", &new_version);
-            try!(save_version(&meta_f_p, new_version));
-        } else {
-            say("white", "Version already updated (");
-            say("magenta", &pipeline_v);
-            say("white", "/");
-            say("green", &current_v);
-            sayln("white", ")");
-        }
-    }
-    Ok(())
-}
-
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
+    use cookbook::*;
 
     #[test]
     fn verify_version_bumping_using_bump_patchset() {
-        let version = String::from("1.2.3");
-        assert_eq!(String::from("1.2.4"), super::bump_patchset(version).unwrap());
+        let version = MetadataVersion { major: 1, minor: 2, patch: 3 };
+        let MetadataVersion { major: ma, minor: mi, patch: pa } = super::bump_patchset(version).unwrap();
+        assert_eq!(ma, 1);
+        assert_eq!(mi, 2);
+        assert_eq!(pa, 4);
     }
 
     #[test]
     fn return_the_metadata_file_path() {
         let project_path = PathBuf::from("/cookbook");
         assert_eq!(String::from("/cookbook/metadata.rb"), super::metadata_file(&project_path));
+    }
+
+    #[test]
+    fn verify_happy_metadata_version_from_content() {
+        let happy_version = "1.2.3";
+        let happy_metadata_content = metadata_from_version(&happy_version);
+        let happy_metadata_version = super::metadata_version_from(&happy_metadata_content).unwrap();
+        assert_eq!(String::from(happy_version), happy_metadata_version.to_string());
+
+        let valid_version = "1.2";
+        let valid_metadata_content = metadata_from_version(&valid_version);
+        let valid_metadata_version = super::metadata_version_from(&valid_metadata_content).unwrap();
+        assert_eq!(String::from("1.2.0"), valid_metadata_version.to_string());
+
+        let awesome_version = "123.123.123";
+        let awesome_metadata_content = metadata_from_version(&awesome_version);
+        let awesome_metadata_version = super::metadata_version_from(&awesome_metadata_content).unwrap();
+        assert_eq!(String::from(awesome_version), awesome_metadata_version.to_string());
+    }
+
+    #[test]
+    fn verify_sad_metadata_version_from_content() {
+        let sad_version = "1..";
+        let sad_metadata =  metadata_from_version(&sad_version);
+        match super::metadata_version_from(&sad_metadata) {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true)
+        }
+
+        let typo_version = "1.2.";
+        let typo_metadata =  metadata_from_version(&typo_version);
+        match super::metadata_version_from(&typo_metadata) {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true)
+        }
+
+        let no_version = "";
+        let no_metadata =  metadata_from_version(&no_version);
+        match super::metadata_version_from(&no_metadata) {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true)
+        }
+    }
+
+    // Quick helper to render a dummy metadata from a provided version
+    fn metadata_from_version(version: &str) -> String {
+        String::from(["version '", version, "'"].concat())
     }
 }
