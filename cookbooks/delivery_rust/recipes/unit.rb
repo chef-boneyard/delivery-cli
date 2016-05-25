c5d6683d97fdfcb080f4d8e9fd327db37b9890fc
Tyler Cloke <tylercloke@gmail.com>
2016-05-25 15:01:46 -0700

    Use correct path to openssl libs in various situations.

--- cookbooks/delivery_rust/recipes/unit.rb
+++ cookbooks/delivery_rust/recipes/unit.rb
@@ -11,7 +11,14 @@ rust_execute 'cargo test' do
   )
 end
 
-ruby_execute 'make cucumber' do
+rust_execute 'cargo build --release' do
+  version node['delivery_rust']['rust_version']
+  cwd node['delivery_builder']['repo']
+  environment( node['delivery_builder']['cargo_env'] )
+end
+
+ruby_execute 'bundle install --binstubs=bin --path=vendor/bundle && bin/cucumber 2>/dev/null && rm -rf features/tmp' do
   version node['delivery_rust']['ruby_version']
   cwd node['delivery_builder']['repo']
 end
+
