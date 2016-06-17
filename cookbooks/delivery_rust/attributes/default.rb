a787c5fb3d3badcd132715c21fb2e91a942b93a1
tyler-ball <tyleraball@gmail.com>
2016-06-17 16:28:38 -0600

    Updating rust version to 1.9.0

--- cookbooks/delivery_rust/attributes/default.rb
+++ cookbooks/delivery_rust/attributes/default.rb
@@ -1,6 +1,6 @@
 default['delivery_rust']['ruby_version'] = '2.1.5'
 
-rust_version = '1.8.0'
+rust_version = '1.9.0'
 time = Time.now.utc.strftime("+%Y-%m-%dT%H:%M:%SZ")
 default['delivery_rust']['rust_version'] = rust_version
 default['delivery_rust']['cargo_env'] = {
@@ -15,4 +15,3 @@ if platform_family == 'windows'
   # Working around a weird bug in the 7-zip cookbook where sometimes the installer ignores INSTALLDIR
   override['7-zip']['home'] = 'C:\\Program Files\\7-Zip'
 end
-
