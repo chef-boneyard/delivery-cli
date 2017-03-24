3a437009fb2064e9cf94544ab7948558e59d472c
Salim Afiune <afiune@chef.io>
2017-03-24 12:12:12 -0400

    Added User module
    This new module is intendent to be use to load Users from Chef Automate,
    the main purpose is for verification but it might be for some use in the
    future.
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

---
+++ src/delivery/user/mod.rs
@@ -0,0 +1,101 @@
+//
+// Copyright:: Copyright (c) 2017 Chef Software, Inc.
+// License:: Apache License, Version 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use types::DeliveryResult;
+use http::APIClient;
+use config::Config;
+use serde_json;
+
+#[derive(Deserialize, Debug)]
+pub struct User {
+  pub first: String,
+  pub last: String,
+  pub name: String,
+  pub email: String,
+  pub ssh_pub_key: String,
+  pub user_type: String,
+}
+
+impl Default for User {
+    fn default() -> Self {
+        User {
+          first: String::from(""),
+          last: String::from(""),
+          name: String::from(""),
+          email: String::from(""),
+          ssh_pub_key: String::from(""),
+          user_type: String::from("internal"),
+        }
+    }
+}
+
+impl User {
+    // Load a `User` from the specified `Config`, by default it will try to load
+    // the user specified in the config but if a different user is provided, it will
+    // load that user instead.
+    //
+    // # Example:
+    //
+    // ```
+    // use delivery::user::User;
+    //
+    // let mine: User = User::load(&config, None);          <- Load user from config
+    // let dif:  User = User::load(&config, Some("link"));  <- Load user `link`
+    // ```
+    pub fn load(config: &Config, user: Option<&str>) -> DeliveryResult<Self> {
+        let u = config.user()?;
+        let name = user.unwrap_or(u.as_str());
+        let mut raw_json = APIClient::from_config(config)?.get(&format!("users/{}", name))?;
+        let json = APIClient::extract_pretty_json(&mut raw_json)?;
+        let user: User = serde_json::from_str(&json)?;
+        Ok(user)
+    }
+
+    pub fn verify(&self) -> bool {
+        if self.ssh_pub_key.is_empty() {
+            return false
+        }
+        true
+    }
+
+    pub fn set_ssh_pub_key(&mut self, key: &str) {
+        self.ssh_pub_key = String::from(key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_a_user_with_defaults() {
+        let u = User::default();
+        assert_eq!(u.first, "".to_string());
+        assert_eq!(u.user_type, "internal".to_string());
+    }
+
+    #[test]
+    fn user_verify() {
+        // ssh_pub_key is not set by default
+        let mut u = User::default();
+        assert!(!u.verify());
+
+        // Setting ssh_pub_key, and then verifying
+        u.set_ssh_pub_key("SECRET");
+        assert!(u.verify());
+    }
+}
